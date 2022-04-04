5c2d3454696801f3e77eb201a85f99661fe1140b
Sarthak Dhakal <sarthakdhakal03@gmail.com>
2022-04-05 00:15:48 +0545

    first commit continued

---
+++ Dissertation/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Dissertation.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
